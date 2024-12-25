08a01c852b95a28a73284549ece95f1c0a818b54
Akheheh <lx.heidarian@gmail.com>
2024-12-25 11:05:08 -0800

    ebo

--- src/main.cpp
+++ src/main.cpp
@@ -46,90 +46,55 @@ auto [WIDTH, HEIGHT, CLEAR_COLOR] = GLView;
 
 int main()
 {
-    //Create a GLFW window which loads GLAD and has resizing
+    // Create a GLFW window which loads GLAD and has resizing
     GLFWwindow *window = createOpenGLEnv(1920, 1080, "GL");
 
-    // vertex data
-    /*
+    // Vertex data
     float vertices[] = {
         -0.5f, -0.5f, 0.0f,
         0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f};
+
+    // Vertex color data
+    float vertexColors[] = {
         1.0f, 0.0f, 0.0f,
         0.0f, 1.0f, 0.0f,
         0.0f, 0.0f, 1.0f};
-*/
-   /*float vertices[] = {
-    // first triangle
-     0.5f,  0.5f, 0.0f,  // top right
-     0.5f, -0.5f, 0.0f,  // bottom right
-    -0.5f,  0.5f, 0.0f,  // top left 
-    // second triangle
-     0.5f, -0.5f, 0.0f,  // bottom right
-    -0.5f, -0.5f, 0.0f,  // bottom left
-    -0.5f,  0.5f, 0.0f   // top left
-};*/
 
-float vertices[] = {
-    0.5f,  0.5f, 0.0f,  // top right
-     0.5f, -0.5f, 0.0f,  // bottom right
-     -0.5f, -0.5f, 0.0f,  // bottom left
-    -0.5f,  0.5f, 0.0f, // top left
+    // Create 2 VBO's
+    GLuint VBOs[2];
+    // Create 2 buffers, and array to store the ID references for each buffer
+    glGenBuffers(2, VBOs);
 
-    //gradients
-    1.0f, 0.0f, 0.0f,
-    0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, 1.0f
-};
+    // Bind the the first new buffer to GL_ARRAY_BUFFER
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
 
-/*float colors[] = {
-    1.0f, 0.0f, 0.0f,
-    0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, 1.0f
-};*/
+    // Load data to the GL_ARRAY_BUFFER binding
+    // glBufferData(target, how much data, the data itself, a hint on how to use the data for optimization uses)
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-//The top left and bottom right vertices are duplicates
-//The square has 2 triangles therefore 6 vertices, when we only need 4
-//We want to draw only the vertices as needed
-//Create an element buffer object or EBO to store indices of unique vertices and tell OpenGL when to draw them
-GLuint EBO;
-glGenBuffers(1, &EBO);
+    // Repeat for the second buffer, bind to GL_ARRAY_BUFFER and load data
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexColors), vertexColors, GL_STATIC_DRAW);
 
-GLuint indices[] = {
-    //first triangle
-    0, 1, 3,
-
-    //second triangle
-    1, 2, 3
-};
-
-
-
-    // Create and bind the Vertex Array
+    // Create a VAO
     GLuint VAO;
+    // Create 1 vertex array, with an array to store the ID references of each vertex array
     glGenVertexArrays(1, &VAO);
-
+    // Bind the VAO for drawing
     glBindVertexArray(VAO);
 
-    // Create and bind the Vertex Buffer
-    GLuint VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    // Load vertext data into the vertex buffer
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    //This time we bind our EBO to the target GL_ELEMENT_ARRAY_BUFFER
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    //Load the data into the buffer
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // Tell the Vertex Array how to read data for the given attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)(0));
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)(12 * sizeof(float)));
-
-    // Turn on the attribute
+    // Tell OpenGL how to interpret the buffer data for each attribute
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), (void *)(0));
     glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+    glVertexAttribPointer(1, 3, GL_FLOAT, false, 3 * sizeof(float), (void *)(0));
     glEnableVertexAttribArray(1);
+
+    std::cout << VAO << std::endl;
+    std::cout << &VAO << std::endl;
 
     // Create a vertex shader, fragment shader, and a shader program
     GLuint vertexShader, fragmentShader, shaderProgram;
@@ -164,13 +129,17 @@ GLuint indices[] = {
 
         // Draw the triangle
         glUseProgram(shaderProgram);
+        // While it's technically not necessary to call glBindVertexArray() for this example,
+        // We should anyway, because if we have multiple VAOs then we will introduce unintended behavior
+        // AKA we might be drawing from a different VAO than we intended, or using a library which has its own VAOs
         glBindVertexArray(VAO);
-        //glDrawArrays(GL_TRIANGLES, 0, 3);
-        //glDrawElements() indicates we want to draw from an index buffer, aka the GL_ELEMENT_ARRAY_BUFFER
-        //We want to draw from 6 indices which are of GLUint, with 0 offset from the element buffer
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        //Tell OpenGL to draw the triangles as lines (GL_LINE) and to do so for both the front and back of each triangle (GL_FRONT AND BACK)
-        //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glDrawElements() indicates we want to draw from an index buffer, aka the GL_ELEMENT_ARRAY_BUFFER
+        // We want to draw from 6 indices which are of GLUint, with 0 offset from the element buffer
+
+        // Tell OpenGL to draw the triangles as lines (GL_LINE) and to do so for both the front and back of each triangle (GL_FRONT AND BACK)
+        // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
